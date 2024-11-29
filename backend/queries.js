ad33092a925a25e18c88afea5ce0fea251c6c9f3
sanchocreativo <sanchocreativo@gmail.com>
2024-11-29 18:15:04 -0300

    etwat

--- backend/queries.js
+++ backend/queries.js
@@ -15,7 +15,7 @@ const getUsers = async (req, res) => {
     res.status(200).json(results.rows);
   } catch (error) {
     console.error("Error fetching users:", error);
-    res.status(500).send("Error fetching users");
+    res.status(500).send(`Error fetching users test ${error}`);
   }
 };
 
